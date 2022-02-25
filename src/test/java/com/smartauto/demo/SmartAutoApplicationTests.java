b3699fb5a183398890d36002eace0be57f146e72
Vishnu <vishnusr01@gmail.com>
2022-02-25 11:45:40 +0530

    Add test folder

---
+++ src/test/java/com/smartauto/demo/SmartAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartauto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
