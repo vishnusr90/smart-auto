c951cf37a2926ba2a7fbde63366085d2540a3f42
Vishnu <vishnusr01@gmail.com>
2022-02-25 11:47:00 +0530

    Add entry

---
+++ src/main/java/com/smartauto/demo/SmartAutoApplication.java
@@ -0,0 +1,13 @@
+package com.smartauto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAutoApplication.class, args);
+	}
+	
+}
