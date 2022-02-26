9d4bbd09e990d7dc10ec5920a91fa07a86a56668
Vishnu <vishnusr01@gmail.com>
2022-02-26 23:17:48 +0530

    Optimize imports

--- src/main/java/com/smartauto/demo/controller/SalesController.java
+++ src/main/java/com/smartauto/demo/controller/SalesController.java
@@ -2,14 +2,11 @@ package com.smartauto.demo.controller;
 
 import java.security.Principal;
 
-import com.smartauto.demo.repository.dto.CarDTO;
 import com.smartauto.demo.service.SalesService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
