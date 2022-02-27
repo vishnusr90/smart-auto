7fb8e1b37f4833df01325b24b5cd38380357733a
Vishnu <vishnusr01@gmail.com>
2022-02-27 13:39:41 +0530

    Remove console logs

--- src/main/webapp/service/index-service.js
+++ src/main/webapp/service/index-service.js
@@ -2,7 +2,6 @@ import '../js/components/car-list-container/car-list-container.js';
 import { getCurrentRole } from './user-service.js';
 
 (async() => {
-    console.log('this');
     const container = document.querySelector('car-list-container');
     container.load();
 })();
