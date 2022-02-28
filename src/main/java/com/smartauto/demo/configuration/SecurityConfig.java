93ade3320c35b058a3c2112db4641ef1de760c1b
Vishnu <vishnusr01@gmail.com>
2022-02-28 08:06:45 +0530

    Remove comments

--- src/main/java/com/smartauto/demo/configuration/SecurityConfig.java
+++ src/main/java/com/smartauto/demo/configuration/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.smartauto.demo.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,7 +8,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true) // for preAuthorize
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override  
