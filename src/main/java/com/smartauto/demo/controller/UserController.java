4bbad577f1318a5a688163d49413014b35660425
Vishnu <vishnusr01@gmail.com>
2022-02-28 07:21:47 +0530

    Remove unnecessary files

--- src/main/java/com/smartauto/demo/controller/UserController.java
+++ src/main/java/com/smartauto/demo/controller/UserController.java
@@ -2,27 +2,18 @@ package com.smartauto.demo.controller;
 
 import java.util.*;
 
-import com.smartauto.demo.repository.dto.UserDTO;
-import com.smartauto.demo.service.UserService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    @Autowired
-    private UserService userService;  
     
     @GetMapping("/role")
     public Collection<? extends GrantedAuthority> get(Authentication authentication) {
         return authentication.getAuthorities();
     }
-
 }
